6c88aa3e39a0f2f07f831289bc8a8d87819cf0a3
Erik Berger <berger.erik12@gmail.com>
2024-07-29 17:47:17 +0200

    Change my url paths for static and added include for products app

--- product_comparison/urls.py
+++ product_comparison/urls.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
-from .views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index'),
-    path('api/', include('products.urls')),
+    path('products/', include('products.urls')),
+    path('', TemplateView.as_view(template_name='index.html')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
