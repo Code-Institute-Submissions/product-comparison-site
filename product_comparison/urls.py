6ecee9d7362028b6fc6606ec7887ee06d25b5e7d
Erik Berger <berger.erik12@gmail.com>
2024-07-29 15:42:04 +0200

    Updated urlpatterns to include the static files and the view for React index page

--- product_comparison/urls.py
+++ product_comparison/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
+from .views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='index.html')),
+    path('', index, name='index'),
     path('api/', include('products.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
