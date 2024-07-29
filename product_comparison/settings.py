19a5ffb91a57290327cb7a661bbb386822074f95
Erik Berger <berger.erik12@gmail.com>
2024-07-29 19:09:23 +0200

    Changed static files path in settings

--- product_comparison/settings.py
+++ product_comparison/settings.py
@@ -49,7 +49,7 @@ ROOT_URLCONF = 'product_comparison.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'frontend/build')],
+        'DIRS': [os.path.join(BASE_DIR, 'staticfiles')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
