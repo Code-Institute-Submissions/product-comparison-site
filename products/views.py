e4704e61660dce8af93296dcfc09ea48b2ebadbe
Erik Berger <berger.erik12@gmail.com>
2024-07-30 15:37:04 +0200

    Updated view sets

--- products/views.py
+++ products/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Product, Category, Comment, Vote
+from .serializers import ProductSerializer, CategorySerializer, CommentSerializer, VoteSerializer
 
 
-def home(request):
-    return render(request, 'index.html')
+# def home(request):
+#     return render(request, 'index.html')
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+
+class VoteViewSet(viewsets.ModelViewSet):
+    queryset = Vote.objects.all()
+    serializer_class = VoteSerializer
