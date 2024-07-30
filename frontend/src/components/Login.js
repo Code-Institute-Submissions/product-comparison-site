216b98d88a98a36b9f487d9936ff970efe81a43a
Erik Berger <berger.erik12@gmail.com>
2024-07-30 15:38:07 +0200

    Added login.js

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios.post('http://localhost:8000/api/token/', { username, password })
+            .then(response => {
+                localStorage.setItem('token', response.data.access);
+            })
+            .catch(error => console.error(error));
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                placeholder="Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+            />
+            <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <button type="submit">Login</button>
+        </form>
+    );
+};
+
+export default Login;
