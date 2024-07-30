2297b6035183c544135b2d7e80fcce78cfac963e
Erik Berger <berger.erik12@gmail.com>
2024-07-30 15:36:54 +0200

    Updated url paths

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,21 @@
-from django.urls import path
-from . import views
+# from django.urls import path
+# from . import views
+
+# urlpatterns = [
+#   path('', views.home, name='home'),
+# ]
+
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ProductViewSet, CategoryViewSet, CommentViewSet, VoteViewSet
+
+router = DefaultRouter()
+router.register(r'products', ProductViewSet)
+router.register(r'categories', CategoryViewSet)
+router.register(r'comments', CommentViewSet)
+router.register(r'votes', VoteViewSet)
 
 urlpatterns = [
-  path('', views.home, name='home'),
+    path('', include(router.urls)),
 ]
