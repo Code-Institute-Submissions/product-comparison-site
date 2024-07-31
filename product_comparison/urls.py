27d607d4870bf4afa587ee0f22427bc48256ee52
Erik Berger <berger.erik12@gmail.com>
2024-07-31 14:39:31 +0200

    Including allauth URLs in projects path

--- product_comparison/urls.py
+++ product_comparison/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('products/', include('products.urls')),
+    path('accounts/', include('allauth.urls')),
     path('api/', include('products.urls')),
     path('', TemplateView.as_view(template_name='index.html')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
