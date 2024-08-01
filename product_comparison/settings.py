fd34e42bfcd17303b46b5164eeb5e18ca5962b15
Erik Berger <berger.erik12@gmail.com>
2024-08-01 13:24:04 +0200

    Authentication for backend

--- product_comparison/settings.py
+++ product_comparison/settings.py
@@ -36,6 +36,10 @@ INSTALLED_APPS = [
     'corsheaders',
     'products',
 ]
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
 
 SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
