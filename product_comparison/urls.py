58f3442391678c17f9e01f72d601521b556c4c34
Erik Berger <berger.erik12@gmail.com>
2024-08-05 13:22:46 +0200

    Updated url paths for new login system

--- product_comparison/urls.py
+++ product_comparison/urls.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
+    path('api/', include('user_api.urls')),
     path('api/', include('products.urls')),
-    path('', TemplateView.as_view(template_name='index.html')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    # Catch all other paths and serve the React app
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
