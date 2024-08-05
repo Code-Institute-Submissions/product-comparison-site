a324b913e334ca0019198862042f6317eee31f9c
Erik Berger <berger.erik12@gmail.com>
2024-08-05 15:12:03 +0200

    Removed console.log for testing

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-console.log('API_URL:', process.env.REACT_APP_API_URL);
-
 const API_URL = process.env.REACT_APP_API_URL || "http://localhost:8000/api/";
 
 const register = (username, password) => {
