6ce69b769fadaab6aeefecf1c68df0b3f2cbed7f
Erik Berger <berger.erik12@gmail.com>
2024-08-06 15:35:05 +0200

    Added static media url to paths

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ProductListView, ProductCreateView, ProductDetailView, CategoryViewSet, CommentViewSet, VoteViewSet
+from django.conf import settings
+from django.conf.urls.static import static
 
 # Define the router and register viewsets
 router = DefaultRouter()
@@ -14,4 +16,4 @@ urlpatterns = [
     path('products/', ProductListView.as_view(), name='product-list'),
     path('products/create/', ProductCreateView.as_view(), name='product-create'),
     path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
