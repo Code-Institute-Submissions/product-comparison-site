d09119cb6f8149951a07708a52edb628b3037b9f
Erik Berger <berger.erik12@gmail.com>
2024-08-06 14:04:21 +0200

    Updated my products views

--- products/views.py
+++ products/views.py
@@ -1,21 +1,29 @@
-# from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import generics, viewsets
 from .models import Product, Category, Comment, Vote
 from .serializers import ProductSerializer, CategorySerializer, CommentSerializer, VoteSerializer
-
-
-# def home(request):
-#     return render(request, 'index.html')
-
-
-class ProductViewSet(viewsets.ModelViewSet):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+
+class ProductListView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductCreateView(generics.CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticated, IsAdminUser]
+
+
+class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticated, IsAdminUser]
 
 
 class CommentViewSet(viewsets.ModelViewSet):
