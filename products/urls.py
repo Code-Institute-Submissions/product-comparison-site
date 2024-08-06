00ba97f228433b5b0f948b662aaca52ba954be92
Erik Berger <berger.erik12@gmail.com>
2024-08-06 14:04:48 +0200

    Updated url paths for products

--- products/urls.py
+++ products/urls.py
@@ -1,21 +1,17 @@
-# from django.urls import path
-# from . import views
-
-# urlpatterns = [
-#   path('', views.home, name='home'),
-# ]
-
-
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ProductViewSet, CategoryViewSet, CommentViewSet, VoteViewSet
+from .views import ProductListView, ProductCreateView, ProductDetailView, CategoryViewSet, CommentViewSet, VoteViewSet
 
+# Define the router and register viewsets
 router = DefaultRouter()
-router.register(r'products', ProductViewSet)
 router.register(r'categories', CategoryViewSet)
 router.register(r'comments', CommentViewSet)
 router.register(r'votes', VoteViewSet)
 
+# Define URL patterns, combining router with individual generic views
 urlpatterns = [
     path('', include(router.urls)),
+    path('products/', ProductListView.as_view(), name='product-list'),
+    path('products/create/', ProductCreateView.as_view(), name='product-create'),
+    path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
 ]
